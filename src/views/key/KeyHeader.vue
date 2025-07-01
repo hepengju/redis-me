bf01c3e87eae35b411f0661ade5f8ecab448fa6c
hepengju <he_pengju@163.com>
2025-07-01 11:21:22 +0800

    无连接时禁用部分按钮

--- src/views/key/KeyHeader.vue
+++ src/views/key/KeyHeader.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import MeIcon from '@/components/MeIcon.vue'
 import store, {colorStyle, initMain} from '@/utils/store.ts'
-
-// 新增连接
-function addConn(){
-
-}
+const disabled = computed(() => store.conn === null)
 
 // 扩展命令
 function handleCommand(command: string) {
@@ -34,19 +30,18 @@ function handleCommand(command: string) {
     </el-select>
 
     <div class="btns">
-      <me-button content="新增连接" type="primary" icon="el-icon-plus" @click="addConn"></me-button>
-
-      <el-dropdown style="margin-left: 10px" placement="bottom-end" @command="handleCommand">
+      <el-dropdown placement="bottom-end" @command="handleCommand">
         <el-button type="success" icon="el-icon-operation"/>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item command="refreshConn"><me-icon name="刷新连接" icon="el-icon-refresh"/></el-dropdown-item>
-            <el-dropdown-item command="editConn"><me-icon name="编辑连接" icon="el-icon-edit"/></el-dropdown-item>
-            <el-dropdown-item command="deleteConn"><me-icon name="删除连接" icon="el-icon-delete"/></el-dropdown-item>
-            <el-dropdown-item command="import" divided><me-icon name="导入键" icon="el-icon-download"/></el-dropdown-item>
-            <el-dropdown-item command="flush"><me-icon name="删除所有键" icon="el-icon-warn-triangle-filled"/></el-dropdown-item>
-            <el-dropdown-item command="appLog" divided><me-icon name="应用日志" icon="el-icon-stopwatch"/></el-dropdown-item>
-            <el-dropdown-item command="setting" divided><me-icon name="基础设置" icon="el-icon-setting"/></el-dropdown-item>
+            <el-dropdown-item command="refreshConn" :disabled><me-icon name="刷新连接" icon="el-icon-refresh"/></el-dropdown-item>
+            <el-dropdown-item command="addConn"><me-icon name="新增连接" icon="el-icon-plus"/></el-dropdown-item>
+            <el-dropdown-item command="editConn" :disabled><me-icon name="编辑连接" icon="el-icon-edit"/></el-dropdown-item>
+            <el-dropdown-item command="deleteConn" :disabled><me-icon name="删除连接" icon="el-icon-delete"/></el-dropdown-item>
+            <el-dropdown-item command="import" divided :disabled><me-icon name="导入键" icon="el-icon-download"/></el-dropdown-item>
+            <el-dropdown-item command="flush" :disabled><me-icon name="删除所有键" icon="el-icon-warn-triangle-filled"/></el-dropdown-item>
+            <el-dropdown-item command="appLog" divided><me-icon name="命令日志" icon="el-icon-stopwatch"/></el-dropdown-item>
+            <el-dropdown-item command="setting"><me-icon name="基础设置" icon="el-icon-setting"/></el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
