d2938dba82ad6174f509870934c85c05b7656244
何鹏举 <he_pengju@163.com>
2025-07-15 23:49:33 +0800

    按钮支持自定义图标

--- src/components/MeButton.vue
+++ src/components/MeButton.vue
@@ -3,12 +3,18 @@ export default {
   props: {
     info: {type: String, default: '请添加按钮提示'},
     placement: {type: String, default: 'auto'},
+    icon: {type: String, default: ''},
   }
 }
 </script>
 
 <template>
   <el-tooltip :content="info" :show-after="500" :placement="placement">
-    <el-button v-bind="$attrs"></el-button>
+    <el-button v-bind="$attrs" :icon="icon" v-if="icon.startsWith('el-icon')"></el-button>
+    <el-button v-bind="$attrs" v-else>
+      <template #icon>
+        <SvgIcon :name="icon" class="icon"/>
+      </template>
+    </el-button>
   </el-tooltip>
 </template>
