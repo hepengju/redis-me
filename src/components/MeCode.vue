a6d9986d065858330f530e601be13a7ff030491b
hepengju <he_pengju@163.com>
2025-07-16 14:52:05 +0800

    me-code 支持自动深浅色主题

--- src/components/MeCode.vue
+++ src/components/MeCode.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
 // https://rennzhang.github.io/codemirror-editor-vue3/zh-CN/guide/getting-started
 import CodeMirror from 'codemirror-editor-vue3'
 
@@ -20,33 +20,27 @@ import 'codemirror/addon/dialog/dialog.js'
 import 'codemirror/addon/dialog/dialog.css'
 import 'codemirror/addon/selection/mark-selection.js'
 
-export default {
-  components: {CodeMirror},
-  props: {
-    mode: {type: String, default: 'application/json'},
-    readOnly: {type: Boolean, default: false, required: false},
-  },
-  data() {
-    return {
-      initOptions : {
-        lineNumbers: true,     // 显示行号
-        scrollbarStyle: null,  // 不显示滚动条
-        styleActiveLine: false, // 高亮当前行
-        border: false
-      }
-    }
-  },
-  computed: {
-    cmOptions() {
-      return ({
-        ...this.initOptions,
-        mode: this.mode,
-        readOnly: this.readOnly,
-        theme: 'monokai'
-      })
-    }
-  }
-}
+const {mode, readOnly} = defineProps({
+  mode: {type: String, default: 'application/json'},
+  readOnly: {type: Boolean, default: false, required: false},
+})
+
+const initOptions = reactive({
+  lineNumbers: true,     // 显示行号
+  scrollbarStyle: null,  // 不显示滚动条
+  styleActiveLine: false, // 高亮当前行
+  border: false,
+})
+
+const isDark = useDark()
+const cmOptions = computed(() => {
+  return ({
+    ...initOptions,
+    mode,
+    readOnly,
+    theme: isDark.value ? 'monokai' : 'idea'
+  })
+})
 </script>
 
 <template>
