4db463b5712bd577121f6270add324038716b278
hepengju <he_pengju@163.com>
2025-07-16 18:09:47 +0800

    redis 值显示优化

--- src/utils/util.js
+++ src/utils/util.js
@@ -34,3 +34,25 @@ export function randomString(n) {
 export function sleep(delay) {
     return new Promise(resolve => setTimeout(resolve, delay));
 }
+
+// 人类可读的大小
+const humanUnits = [
+    {threshold: 1        , symbol: 'B'},
+    {threshold: 1024     , symbol: 'K'},
+    {threshold: 1024 ** 2, symbol: 'M'},
+    {threshold: 1024 ** 3, symbol: 'G'},
+    {threshold: 1024 ** 4, symbol: 'T'},
+]
+export function humanSize(size, zeroShow = '0B', fractionDigits = 2) {
+    if (!size) return zeroShow || ''
+
+    // 从大到小查找合适的单位
+    for (let i = humanUnits.length - 1; i >= 0; i--) {
+        if (size >= humanUnits[i].threshold) {
+            const value = size / humanUnits[i].threshold;
+            return value.toFixed(fractionDigits) + humanUnits[i].symbol;
+        }
+    }
+
+    return size + 'B'; // 小于1KB的情况
+}
--- src/views/tag/RedisValue.vue
+++ src/views/tag/RedisValue.vue
@@ -1,21 +1,49 @@
 <script setup>
 import useGlobalStore from '@/utils/store.js'
 import {capitalize} from 'lodash'
-import {copy} from '@/utils/util.js'
+import {copy, humanSize} from '@/utils/util.js'
 
 // 全局对象
 const global = useGlobalStore()
 
-const showValue = ref('')
-const showSize = ref(0)
-const showSave = ref(true)
-const magicValue = ref(true)
+// 值的显示方式
 const hashKey = ref('')
+const isPretty = ref(true)
+const isHashValue = ref(false)
+const isTable = ref(false)
+
+// 计算属性
+const stringTypeOrHashValue = computed(() => 'string' === global.redisValue?.type || isHashValue.value)
+const showValue = computed(() => {
+  const rv = global.redisValue
+  console.log(rv)
+  console.log(JSON.stringify(rv?.value, null, 2))
+
+  if (isPretty.value) {
+    if (stringTypeOrHashValue.value) {
+      const str = rv?.value?.toString()
+      try {
+        return str.startsWith('{') || str.startsWith('[') ? JSON.stringify(JSON.parse(str), null, 2) : str
+      } catch (e) {
+        return str
+      }
+    } else {
+      return JSON.stringify(rv?.value, null, 2)
+    }
+  } else {
+    return 'hash' === rv?.type && !isHashValue.value ? JSON.stringify(rv.value) : rv.value?.toString()
+  }
+})
+
+
+function showEdit(){
+
+}
 function expireKey(){
   console.log('expireKey')
 }
 function getValue(){}
-function setKey(){}
+function setValue(){}
 function delKey(){}
 </script>
 
@@ -43,7 +71,9 @@ function delKey(){}
           <el-input v-model.number="global.redisValue.ttl" style="width: 170px; margin: 0 10px;">
             <template #prepend>TTL</template>
             <template #append v-if="!global.readonly">
-              <me-button info="点击修改键的过期时间（单位为秒，-1代表永久）" icon="el-icon-select" @click="expireKey"
+              <me-button info="点击修改键的过期时间（单位为秒，-1代表永久）"
+                         icon="el-icon-select" @click="expireKey"
+                         style="color: var(--el-color-danger)"
                          :disabled="!global.redisKey.key" placement="top-end"/>
             </template>
           </el-input>
@@ -57,22 +87,23 @@ function delKey(){}
 
       <div class="value">
         <me-code :value="showValue" @update:value="(newValue) => global.redisValue.newValue=newValue"
-                 mode="application/json" :read-only="!showSave"></me-code>
+                 mode="application/json" :read-only="global.readonly || !stringTypeOrHashValue"></me-code>
         <el-button-group class="btn">
-          <el-button>Size: {{ showSize }}</el-button>
+          <el-button>Size: {{ humanSize(global.redisValue.rawValue.length) }}</el-button>
           <me-button info="复制" icon="el-icon-document-copy" @click="copy(showValue)" placement="bottom"/>
           <me-button info="默认开启美化，开启后针对hash/list/set/json等进行格式化，关闭后显示原始值toString"
                      placement="bottom-end"
                      icon="el-icon-magic-stick"
-                     :style="magicValue ? {'backgroundColor': '#f4de29 !important', 'color': 'black' } : {}"
-                     @click="magicValue = !magicValue"/>
+                     :style="isPretty ? {'backgroundColor': '#f4de29 !important', 'color': 'black' } : {}"
+                     @click="isPretty = !isPretty"/>
         </el-button-group>
+
         <el-button-group class="save" v-if="!global.readonly">
-          <me-button type="danger" icon="me-icon-save" v-if="showSave" @click="setKey" placement="top"
-                     info="保存"></me-button>
-          <!-- TODO 编辑Hash
-          <el-button type="danger" icon="el-icon-edit" v-if="showEdit" @click="editHash"></el-button>
-          -->
+          <me-button info="保存" placement="top" type="danger" icon="me-icon-save" @click="setValue"
+                     v-if="stringTypeOrHashValue"/>
+          <me-button info="切换表格显示" placement="top" icon="el-icon-edit" @click="showEdit" v-else
+                     :style="isTable ? {'backgroundColor': '#f4de29 !important', 'color': 'black' } : {}"
+          />
         </el-button-group>
       </div>
     </template>
@@ -107,14 +138,12 @@ function delKey(){}
     overflow: hidden;
 
     .btn {
-      background-color: #272822;
       position: absolute;
       right: 0;
       top: 0;
     }
 
     .save {
-      background-color: #272822;
       position: absolute;
       right: 0;
       bottom: 0;
