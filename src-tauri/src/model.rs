23796870ed99ac7fa548e37376c11df69aac1d66
何鹏举 <he_pengju@163.com>
2025-09-27 14:20:45 +0800

    新增api_model!宏简化代码

--- src-tauri/src/model.rs
+++ src-tauri/src/model.rs
@@ -1,51 +1,45 @@
+use std::any::Any;
 use serde::{Deserialize, Serialize};
 
-// 连接
-// #[derive(Debug, Builder, Deserialize, Serialize)]
-// pub struct Connection {
-//     name: String,
-//
-//     ip: String,
-//     port: i32,
-//     username: Option<String>,
-//     password: Option<String>,
-//
-//     ssh: Option<bool>,
-//     ssl: Option<bool>,
-//     sentinel: Option<bool>,
-//     cluster: Option<bool>,
-// }
-#[derive(Debug, Deserialize, Serialize, Default)]
-pub struct RedisNode {
-    pub id: String,
-    pub node: String,
-    pub is_master: bool,
-    pub slave_of_node: Option<String>
+// 创建公共宏: 简化模型定义（DeepSeek生成）
+#[macro_export]
+macro_rules! api_model {
+    ($struct:ident { $($field:ident : $type:ty),+ $(,)? }) => {
+        #[derive(Serialize, Deserialize, Debug, Clone)]
+        #[serde(rename_all = "camelCase")]
+        pub struct $struct {
+            $(pub $field: $type),+
+        }
+    };
 }
 
-#[derive(Debug, Deserialize, Serialize, Default)]
-pub struct ScanParam {
-    pub id: String,
+// 集群节点
+api_model!( RedisNode {
+    id: String,
+    node: String,
+    is_master: bool,
+    slave_of_node: Option<String>
+});
 
-    // #[serde(default = "*")]
-    pub pattern: String,
+// 扫描参数
+api_model!( ScanParam {
+    id: String,
+    pattern: String,
+    count: usize,
+    scan_type: Option<String>,
+});
 
-    // #[serde(default = "1000")]
-    pub count: usize,
-
-    pub scan_type: Option<String>,
+// Redis键: 由于键是字节存储的，考虑转换为utf-8字符串显示后可能会丢失信息，因此封装为对象
+api_model!( RedisKey {
+    key: String,    // 显示
+    bytes: Vec<u8>, // 修改、删除等依据
 }
+);
 
-/// Redis键: 由于键是字节存储的，考虑转换为utf-8字符串显示后可能会丢失信息，因此封装为对象
-#[derive(Debug, Deserialize, Serialize)]
-pub struct RedisKey {
-    pub key: String,    // 显示
-    pub bytes: Vec<u8>, // 修改、删除等依据
-}
-
-pub struct RedisValue {
-    // pub _type: String,
-    // pub ttl: u64,
-    // pub value: Object,
-    // pub rawValue:
-}
+// Redis值
+api_model!( RedisValue {
+    key_type: String,
+    ttl: u64,
+    value: dyn Any,
+    rawValue: Vec<u8>,
+});
