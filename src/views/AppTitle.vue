bc2a730a23b9216812dd9362e45faea3aa5c82a5
hepengju <he_pengju@163.com>
2025-11-05 15:29:22 +0800

    优化窗口最大化监听，确保图标显示正确

--- src/views/AppTitle.vue
+++ src/views/AppTitle.vue
@@ -1,24 +1,17 @@
 <script setup>
-import { Window } from '@tauri-apps/api/window';
-import {useDark, useToggle} from "@vueuse/core";
+import {Window} from '@tauri-apps/api/window'
+import {useDark, useToggle} from '@vueuse/core'
+import {debounce} from 'lodash'
 
 // 模拟窗口操作
-const appWindow = new Window('main');
+const appWindow = new Window('main')
 const isMaximized = ref(false)
 
-const toMin = () => {
-  appWindow.minimize()
-}
-
-const toMax = async () => {
-  await appWindow.toggleMaximize()
+onMounted(() => window.addEventListener('resize', calcIsMaximized))
+onUnmounted(() => window.removeEventListener('resize', calcIsMaximized))
+const calcIsMaximized = debounce(async () => {
   isMaximized.value = await appWindow.isMaximized()
-  console.log(isMaximized.value)
-}
-
-const toClose = () => {
-  appWindow.close()
-}
+}, 200)
 
 // 主题切换
 const isDark = useDark()
@@ -28,14 +21,15 @@ const toggleDark = useToggle(isDark)
 <template>
   <div data-tauri-drag-region class="title-bar me-flex">
     <div class="me-flex" style="align-items: center">
-      <me-icon icon="me-icon-redis-me" class="icon-btn" style="margin-left: 5px; font-size: 16px;" @click="toggleDark()"/>
+      <me-icon icon="me-icon-redis-me" class="icon-btn" style="margin-left: 5px; font-size: 16px;"
+               @click="toggleDark()"/>
       <div style="margin-left: 5px;font-size: 12px">RedisME</div>
     </div>
     <div style="font-size: 12px;">
-      <me-icon icon="me-icon-window-minimize" class="title-button normal-btn" @click="toMin"/>
-      <me-icon icon="me-icon-window-maximize" class="title-button normal-btn" @click="toMax" v-show="!isMaximized"/>
-      <me-icon icon="me-icon-window-restore"  class="title-button normal-btn" @click="toMax" v-show="isMaximized"/>
-      <me-icon icon="me-icon-window-close"    class="title-button danger-btn" @click="toClose"/>
+      <me-icon icon="me-icon-window-minimize" class="title-button normal-btn" @click="appWindow.minimize()"/>
+      <me-icon icon="me-icon-window-maximize" class="title-button normal-btn" @click="appWindow.toggleMaximize()" v-show="!isMaximized"/>
+      <me-icon icon="me-icon-window-restore"  class="title-button normal-btn" @click="appWindow.toggleMaximize()" v-show="isMaximized"/>
+      <me-icon icon="me-icon-window-close"    class="title-button danger-btn" @click="appWindow.close()"/>
     </div>
   </div>
 </template>
@@ -58,6 +52,7 @@ const toggleDark = useToggle(isDark)
   .normal-btn:hover {
     background: var(--el-color-info-light-7);
   }
+
   .danger-btn:hover {
     background: var(--el-color-danger);
   }
